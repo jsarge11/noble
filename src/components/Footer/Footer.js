ce6c34c975bfe9ade4b5acb223b56b215d775df6
Jay Sargent <sargentjay11@gmail.com>
2018-08-22 10:38:53 -0600

    top nav bar

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,46 +1,70 @@
-import React from 'react'
+import React, { Component } from 'react'
 import logo from '../../assets/images/logo.png'
+import axios from 'axios'
 import './footer.css'
 
 
-export default function Footer(props) {
-    return (
-        <footer className="footer-distributed">
+export default class Footer extends Component {
+    state = {
+        messageSent: false
+    }
 
-        <div className="footer-left">
+    handleInput = (field, e) => {
+        this.setState({ [`${field}`]: e.target.value })
+    }
 
-    <img src={logo} height="50px" alt="logo" />
+    sendMessage = () => {
+        let { name, email, message } = this.state;
+        console.log('post');
+        axios.post('http://sargentassociates.com:8000/email', {name: name, email: email, message: message}).then(() => {
+            this.setState({ messageSent: true })
+        })
+    }
+    render() {
+        return (
+            <footer className="footer-distributed">
+
+            <div className="footer-left">
+
+        <img src={logo} height="50px" alt="logo" />
 
 
 
-    <p className="footer-company-name">
-    Contact Directly:<br/>
-    (801)-935-1273<br/>
-    louie@noblemanagement.net<br/><br/>
-    Noble Stone Management &copy; 2018</p>
+        <p className="footer-company-name">
+        Contact Directly:<br/>
+        (801)-935-1273<br/>
+        louie@noblemanagement.net<br/><br/>
+        Noble Stone Management &copy; 2018</p>
 
-    {/* <div className="footer-icons">
-        <a href="#"><i className="fa fa-facebook"></i></a>
-        <a href="#"><i className="fa fa-twitter"></i></a>
-        <a href="#"><i className="fa fa-linkedin"></i></a>
-    </div> */}
+        {/* <div className="footer-icons">
+            <a href="#"><i className="fa fa-facebook"></i></a>
+            <a href="#"><i className="fa fa-twitter"></i></a>
+            <a href="#"><i className="fa fa-linkedin"></i></a>
+        </div> */}
 
-</div>
+    </div>
 
-<div className="footer-right">
+    <div className="footer-right">
 
-    <p>Inquiries? Message me!</p>
+        <p>Inquiries? Message me!</p>
 
-    <form action="#" method="post">
+        {!this.state.messageSent ?
+            <div className="frontpage-form">
+                <input type="text" name="name" placeholder="Name" onChange={(e) => this.handleInput("name", e)}/>
+                <input type="text" name="email" placeholder="Email" onChange={(e) => this.handleInput("email", e)}/>
+                <textarea name="message" placeholder="Message" onChange={(e) => this.handleInput("message", e)}></textarea>
+                <button onClick={()=>this.sendMessage()}>Send</button>
+            </div >
+        :
+            <div>
+                Thanks for you feedback, we will reach out as soon as we can!
+            </div>
+        }
 
-        <input type="text" name="email" placeholder="Email" />
-        <textarea name="message" placeholder="Message"></textarea>
-        <button>Send</button>
+    </div>
 
-    </form>
+    </footer>
+        )
+    }
 
-</div>
-
-</footer>
-    )
 }
