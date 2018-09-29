038f154f132bfe8c478f85d4bc3533fb3e5520d0
Jay Sargent <sargentjay11@gmail.com>
2018-09-28 22:51:23 -0600

    modal styling

--- src/components/Landing/ServiceAreas/ServiceAreas.js
+++ src/components/Landing/ServiceAreas/ServiceAreas.js
@@ -6,10 +6,13 @@ export default function ServiceAreas(props)  {
         return (
            <div style={{right: props.right}} id="service-area-wrapper">
             <p>We're currently located in the Utah Area, but we're expanding!<br/>
-            Coming soon to ... <br/>
-            &nbsp;Colorado <br/>
-            &nbsp;Arizona <br/>
-            &nbsp;Wyoming</p>
+            Coming soon to ... <br/></p>
+            <p id="coming-soon-areas">
+                &nbsp;<em> CO</em><br/>
+                &nbsp;<em> AZ</em><br/>
+                &nbsp;<em>WY</em><br/>
+            </p>
+            <p> Check back soon for updates on locations! </p>
            </div>
         )
 }
